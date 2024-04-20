a822d4a50ba55eff68d96035297be47ef5d12eff
nebyou16 <nebajhoni22@gmail.com>
2024-04-20 15:24:09 +0300

    updt

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -5,29 +5,34 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
- const [search , setSearch] = useState('');
- const [result , setResult] = useState([]);
-const handleSearch = (e) => {
-  setSearch(e.target.value);
-}
+  const [search, setSearch] = useState('');
+  const [result, setResult] = useState({});
 
-useEffect(() => {
-  axios.get('https://localhost:7089/api/Products')
-    .then(response => {
-      setResult(response.data);     
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}, []);
-
-
+  useEffect(() => {
+    if (search !== '') {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b1fd6e14799699504191b6bdbcadfc35&units=metric`)
+        .then(response => {
+          setResult(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [search]);
+  
 
   return (
     <>
-      <div className='container' value={search} onChange={handleSearch}>
-          <input type="text" /> 
-          <button>Search</button>
+      <div className='container'>
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <button onClick={() => setSearch(search)} >Search</button>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td>{ result.main.humidity}</td>
+          </tr>
+        </table>
       </div>
     </>
   )
